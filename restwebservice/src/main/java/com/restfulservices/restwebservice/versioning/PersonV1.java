b6280824302875003133c74d3bcfbb1f19ebbedf
Echeverria,Oscar <oscar.echeverria@sabre.com>
2022-12-15 09:06:38 -0300

    Material regarding internazationalitaion a versioning control.

---
+++ restwebservice/src/main/java/com/restfulservices/restwebservice/versioning/PersonV1.java
@@ -0,0 +1,11 @@
+package com.restfulservices.restwebservice.versioning;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class PersonV1 {
+
+    String name;
+}
